dbbcee99f87e12b99adc090dd08562dedb5c1a06
Kirill Mangutov <kmangutov@gmail.com>
2022-09-11 18:19:06 -0700

    demo pages

--- js/demo.js
+++ js/demo.js
@@ -7,18 +7,21 @@ new Chart("myChart", {
     datasets: [{
       data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
       borderColor: "red",
-      fill: false
+      fill: false,
+      label: "Lil Nouns"
     },{
       data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
       borderColor: "green",
-      fill: false
+      fill: false,
+      label: "ETH"
     },{
       data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
       borderColor: "blue",
-      fill: false
+      fill: false,
+      label: "USDC"
     }]
   },
   options: {
-    legend: {display: false}
+    legend: {display: true}
   }
 });
